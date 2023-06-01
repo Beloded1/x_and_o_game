552b62e46f4475c3eb440dbdbe83c10023cabe17
Beloded1 <alexandrbeloded@gmail.com>
2023-06-01 14:32:58 +0400

    Add some easy tests.

--- tests/test_x_0_game.py
+++ tests/test_x_0_game.py
@@ -1,7 +1,6 @@
-import pytest
 
-from test_x_0_game import winner
-from test_x_0_game import display_board
+from x_0_game import winner
+from x_0_game import display_board
 
 
 def test__winner__who_is_win():
@@ -18,12 +17,15 @@ def test__winner__if_it_tie():
     assert winner(board) == "TIE"
 
 
-def test__winner__if_no_win():
-    board = ["X", "O", "X",
-             "X", "O", "O",
-             "O", "X", "O"]
-    assert winner(board) == None
-
-def test__display_board:
-    pass
-
+def test__display_board(capsys):
+    board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    display_board(board)
+    captured = capsys.readouterr()
+    expected_output = '''
+    X  | O | X    0  1  2
+    ---+---+---
+    O  | X | O    3  4  5
+    ---+---+---
+    X  | O | X    6  7  8
+    '''
+    assert captured.out.strip() == expected_output.strip()
